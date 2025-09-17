ab582c95ba91171460342fdc9690147fdf09617f
Laboratório FIAP <labsfiap@fiap.com.br>
2025-09-17 10:40:47 -0300

    initial commit

---
+++ src/test/java/br/com/fiap/herosheets/HeroSheetsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.herosheets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeroSheetsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
